b62fb21941b0f93f9e3b1eaf8864c07e3e5491ca
hntlnhh <86276801+hntlnhh@users.noreply.github.com>
2024-06-17 16:42:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Huhu cậu hong quýy tớ, hong chơi zới cậu nữaa 🥺<br>tớ dỗi cậuuu ruii :((";
+  gif.src =
+    "https://media.giphy.com/media/vvc1dJPLEU2QOw9cXy/giphy.gif";
+
+  // Ẩn cả hai nút Yes và No
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+  questionPrompt.style.display = "none"; // Ẩn dòng chữ khi người dùng nhấn vào nút Yes
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
